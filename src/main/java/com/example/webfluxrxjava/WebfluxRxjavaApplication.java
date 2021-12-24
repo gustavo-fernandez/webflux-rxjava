fc56761ae9bbc354f74200ae82d2250aa3a49f1a
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2021-12-23 19:06:14 -0500

    Initial commit

---
+++ src/main/java/com/example/webfluxrxjava/WebfluxRxjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxrxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRxjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxRxjavaApplication.class, args);
+	}
+
+}
